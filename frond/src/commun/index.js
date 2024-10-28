e519fdca140f40115cf13498015e7472b9fabf5e
madenon <brcgr230@gmail.com>
2024-10-28 18:09:05 +0100

    frontend  env and comman change all

--- frond/src/commun/index.js
+++ frond/src/commun/index.js
@@ -1,4 +1,5 @@
-const backendDomain = process.env.REACT_APP_BACKEND_URL // "http://localhost:8000";
+const backendDomain = "http://localhost:8000";
+// const backendDomain = process.env.REACT_APP_BACKEND_URL // "http://localhost:8000"; REACT_APP_BACKEND_URL=http://localhost:8000
 
 const SummaryApi = {
   signUp: {
