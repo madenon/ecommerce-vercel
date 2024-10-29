ba7cfe256c469af9ab714f4aed641cde2f462c65
madenon <brcgr230@gmail.com>
2024-10-29 18:02:41 +0100

    /all-users

--- frond/src/pages/AdminPanel.js
+++ frond/src/pages/AdminPanel.js
@@ -37,8 +37,8 @@ const AdminPanel = () => {
           </div>
           <div>
           <nav className="grid">
-            <Link to={"all-users"} className="px-2 py-1 hover:bg-slate-100">All users </Link>
-            <Link to={"all-products" }className="px-2 py-1 hover:bg-slate-100">Upload product </Link>
+            <Link to={"/all-users"} className="px-2 py-1 hover:bg-slate-100">All users </Link>
+            <Link to={"/all-products" }className="px-2 py-1 hover:bg-slate-100">Upload product </Link>
           </nav>
         </div>
       </aside>
