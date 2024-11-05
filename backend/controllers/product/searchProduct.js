c639e8d760c5fc629ab999ae50808fc81c8899bc
madenon <brcgr230@gmail.com>
2024-11-05 23:06:03 +0100

    delete console.log from search backend

--- backend/controllers/product/searchProduct.js
+++ backend/controllers/product/searchProduct.js
@@ -1,37 +1,33 @@
 import productModel from "../../models/productModel.js";
 
-const searchProduct = async(req, res)=>{
-try {
-    
+const searchProduct = async (req, res) => {
+  try {
     const query = req.query.q;
-    console.log(query)
-    const regex = new RegExp(query,"i","g")
-const product = await productModel.find({
-    "$or":[
+    const regex = new RegExp(query, "i", "g");
+    const product = await productModel.find({
+      $or: [
         {
-            brandName:regex,
+          brandName: regex,
         },
 
         {
-            category:regex
-        }
-    ]
-})
-res.json({
-    data:product,
-    message:"Liste de vos recheches",
-    success:true,
-    error:false
-})
-} catch (error) {
+          category: regex,
+        },
+      ],
+    });
     res.json({
-        message:error.message || error,
-        error:true,
-        success:false
-    })
-    
-}
-}
+      data: product,
+      message: "Liste de vos recheches",
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    res.json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
 
-
-export {searchProduct}
+export { searchProduct };
