7d0bf76fb569b8b95970cecf2a263336a276570e
madenon <brcgr230@gmail.com>
2024-11-10 19:14:34 +0100

    details de payment

--- frond/src/pages/OrderPage.js
+++ frond/src/pages/OrderPage.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import SummaryApi from "../commun";
+import moment from "moment";
+import displayCurrency from "../helpers/displayCurrency";
 
 const OrderPage = () => {
-  return (
-    <div>OrderPage</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default OrderPage
+  const fetchOrderDetails = async () => {
+    const response = await fetch(SummaryApi.getOrder.url, {
+      method: SummaryApi.getOrder.method,
+      credentials: "include",
+    });
+    const responseData = await response.json();
+    setData(responseData.data);
+  };
+
+  useEffect(() => {
+    fetchOrderDetails();
+  }, []);
+
+  return (
+    <div>
+      {!data[0] && <p className="">Vous ne disposez pas d'aucune commande</p>}
+
+      <div>
+        {data.map((item, index) => {
+          return (
+            <div key={item.userId + index}>
+              <p className="font-medium text-lg">
+                {moment(item.createdAt).format("LL")}
+              </p>
+              <div>
+                {item?.productDetails.map((product, index) => {
+                  return (
+                    <div key={product.productId + index}>
+                      <img
+                        src={product?.image[0]}
+                        alt=""
+                        className="w-28 h-28 bg-slate-200 object-scale-down p-2"
+                      />
+                      <div className="">{product.name}</div>
+                      <div className="flex items-center gap-5">
+                        <div className="">{displayCurrency(product.price)}</div>
+                        <div className="">Quantité :{product.quantity}</div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div>
+                <div>
+                  Détails de Payement:
+            </div>
+            </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default OrderPage;
