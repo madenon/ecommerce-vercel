e5fe92fc24299722c88624495a52de8ca1c42764
madenon <brcgr230@gmail.com>
2024-11-10 23:06:59 +0100

    fini

--- backend/controllers/order/orderController.js
+++ backend/controllers/order/orderController.js
@@ -3,7 +3,7 @@ import orderModel from "../../models/orderModel.js";
 const orderController = async (request, response) => {
   try {
     const currentUserId = request.userId;
-    const orderList = await orderModel.find({ userId: currentUserId });
+    const orderList = await orderModel.find({ userId: currentUserId }).sort({createdAt:-1});
     response.json({
       data: orderList,
       message: "Order list",
